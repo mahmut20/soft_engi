7610bc043bfd196b67d7959527ca97df7e1e21c1
Soner Gumus <soner.gumus@outlook.com>
2017-09-29 14:30:57 +0200

    Oplossing 1 toegevoegd

---
+++ opd1/softengineer.lua
@@ -0,0 +1,24 @@
+
+--opdr1
+local input=1000;
+local uitkomst=0;
+
+print("opdr1 =")
+for  i=(input-1),1,-1 
+do
+
+	if i%3==0 or i%5==0 then
+	uitkomst= uitkomst + i;
+	dubbel = 1;
+	
+	
+	end
+	--if i%5==0 then
+	--uitkomst= uitkomst + i;
+	
+	--end
+	
+end	
+	print(uitkomst);
+
+	
